a146fec0bd8a05dff06f8ce7cfdadc02b33dfa9f
Arjun Singh Yadav <arjun@opencraft.com>
2021-08-01 21:56:32 +0530

    Allow cors

--- siosacloud/app.py
+++ siosacloud/app.py
@@ -4,10 +4,24 @@ sys.path.append(os.path.dirname(__file__))
 
 from fastapi import FastAPI
 from api import router
+from fastapi.middleware.cors import CORSMiddleware
 
 from db.mongo import connect_db, close_db
 
 app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(router)
 
